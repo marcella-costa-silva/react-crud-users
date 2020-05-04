c64b029d7a024e89728b7f3885588dddf017f0a5
Marcella <marcellatsangos@gmail.com>
2020-05-04 15:48:20 -0300

    Routes

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
@@ -8,12 +9,14 @@ import './App.css'
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
 import Footer from '../components/template/Footer'
-import Home from '../components/home/Home'
+import Routes from './Routes'
 
 export default props =>
-  <div className="app">
-    <Logo />
-    <Nav />
-    <Home />
-    <Footer />
-  </div>
+  <BrowserRouter>
+    <div className="app">
+      <Logo />
+      <Nav />
+      <Routes />
+      <Footer />
+    </div>
+  </BrowserRouter>
